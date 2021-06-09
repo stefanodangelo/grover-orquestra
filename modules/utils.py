d5228373c99390f1c4a52289f207d815668bac80
stefanodangelo <61756862+stefanodangelo@users.noreply.github.com>
2021-06-09 18:17:20 +0200

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -1,12 +1,14 @@
+"""
 from zquantum.core.circuit import save_circuit
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from math import sqrt
+"""
 import json
-
+"""
 from qiskit import IBMQ, Aer, assemble, transpile
 from qiskit.providers.ibmq import least_busy
 from qiskit.tools.monitor import job_monitor
-
+"""
 def to_zap(circuit, save_path):
     with open(save_path,'w') as f:
         f.write(json.dumps(circuit, indent=2)) # Write message to file as this will serve as output artifact
