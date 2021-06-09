8376aec0401fa1accd4583b54f68f47a493e6ccb
stefanodangelo <61756862+stefanodangelo@users.noreply.github.com>
2021-06-09 11:45:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ import os
 import setuptools
 
 setuptools.setup(
-    name                            = "grover",
-    version                         = "2.0",
+    name                            = "tutorial",
+    version                         = "0.1.0",
+    author                          = "Zapata Computing, Inc.",
+    author_email                    = "info@zapatacomputing.com",
+    description                     = "Training models with scikit-learn in orquestra.",
     url                             = "https://github.com/stefanodangelo/grover-orquestra",
     packages                        = setuptools.find_packages(),
     classifiers                     = (
