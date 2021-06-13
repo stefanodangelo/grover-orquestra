33652facf6dbf6f0f07718562bfbc038c2fa06dd
stefanodangelo <61756862+stefanodangelo@users.noreply.github.com>
2021-06-13 22:12:55 +0200

    add and update

--- steps/build_circuit.py
+++ steps/build_circuit.py
@@ -4,15 +4,7 @@ from zquantum.core.circuit import save_circuit
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from math import sqrt
 
-def build_circuit(n_qubits, save_path='circuit.json'):
-    """
-    message = "Welcome to Orquestra!"
-
-    message_dict = {}
-    message_dict["message"] = message
-    message_dict["schema"] = "message"
-    """
-    
+def build_circuit(n_qubits, save_path='circuit.json'):    
     
     # Define registers
     qr = QuantumRegister(n_qubits)
@@ -31,5 +23,4 @@ def build_circuit(n_qubits, save_path='circuit.json'):
         qc.barrier(qr)
     qc = add_measurements(qc, qr, cr)
     
-    #to_zap(message_dict, save_path)
     to_zap(qc, save_path)
