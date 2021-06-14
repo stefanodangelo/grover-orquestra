2ac1680370d4af5cec6fbc0c168ba53b8932c1f9
stefanodangelo <61756862+stefanodangelo@users.noreply.github.com>
2021-06-14 10:18:10 +0200

    Update measure.py

--- steps/measure.py
+++ steps/measure.py
@@ -1,7 +1,7 @@
 from qiskit import IBMQ, Aer, assemble, transpile
 from qiskit.providers.ibmq import least_busy
 from qiskit.tools.monitor import job_monitor
-from modules.convert import from_json, load_circuit
+from modules.utils import *
 import json
 
 
