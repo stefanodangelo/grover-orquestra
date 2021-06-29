ffec2371b1436fb9afd1fc413175bcba01926061
stefanodangelo <61756862+stefanodangelo@users.noreply.github.com>
2021-06-29 18:34:54 +0200

    Update oracle_expansion.py

--- steps/oracle_expansion.py
+++ steps/oracle_expansion.py
@@ -3,7 +3,7 @@ from modules.utils import *
 from modules.functions import *
 import json 
 
-    
+
 def expand_oracle(circuit, element_to_search, save_path="expanded-circuit.json"):
     element_to_search = element_to_search[::-1] # reverse the string in order to correctly apply the oracle function
     
@@ -14,8 +14,10 @@ def expand_oracle(circuit, element_to_search, save_path="expanded-circuit.json")
     # remove X gates where not needed 
     mcz_positions = find_all_mcz(metadata)
     
+    """
     for position in mcz_positions:
         remove_x_gates(metadata['gates'], position, element_to_search, len(element_to_search))  
-        
+    """
+    
     # save modified .json
     save_circuit(Circuit.from_dict(metadata), save_path)
