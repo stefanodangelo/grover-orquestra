825c79b770fbf4ac3a37c0ce0a670ed3b400bc26
stefanodangelo <61756862+stefanodangelo@users.noreply.github.com>
2021-06-29 11:05:05 +0200

    Create prova.py

---
+++ steps/prova.py
@@ -0,0 +1,29 @@
+from qiskit import IBMQ, Aer, assemble, transpile
+from qiskit.providers.ibmq import least_busy
+from qiskit.tools.monitor import job_monitor
+from modules.utils import *
+import json
+
+
+def run_and_measure(circuit = None, backend=None, save_path = 'measurements.json'):
+	answer = {}
+    answer['schema'] = ""
+    answer['message'] = 'ciao'
+    
+    provider = IBMQ.load_account()
+    """
+    backend = least_busy(provider.backends(filters=lambda x: x.configuration().n_qubits >= len(qc.qubits) and 
+                         not x.configuration().simulator and x.status().operational==True))
+    # Run our circuit on the least busy backend. Monitor the execution of the job in the queue
+    transpiled_grover_circuit = transpile(qc, backend, optimization_level=3)
+    qobj = assemble(transpiled_grover_circuit)
+    job = backend.run(qobj)
+    job_monitor(job, interval=2)
+
+    # Get the results from the computation
+    results = job.result()
+    answer = results.get_counts(qc)
+    """
+    with open(save_path, 'w') as f:
+        json.dump(answer, f)
+       
