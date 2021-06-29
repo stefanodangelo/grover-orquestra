39461556c5e93d2ec6446f6a3b2c5d301952ab21
stefanodangelo <61756862+stefanodangelo@users.noreply.github.com>
2021-06-29 15:09:31 +0200

    Update prova.py

--- steps/prova.py
+++ steps/prova.py
@@ -10,7 +10,7 @@ def run_and_measure(circuit = None, backend=None, save_path = 'measurements.json
     answer['schema'] = ""
 
     IBMQ.save_account("f1b53e81357761bafbbb2d7a71eaa26cfc0d9df4eeb53513096c5cee90ec01bcc8ba6fa547ea4988a1b9136f2abf8090a133546389581b3c51a1471dad8749e6")
-    IBMQ.load_accounts()
+    provider = IBMQ.load_account()
     #backend = least_busy(provider.backends(filters=lambda x: x.configuration().n_qubits >= len(qc.qubits) and not x.configuration().simulator and x.status().operational == True))
     """
     # Run our circuit on the least busy backend. Monitor the execution of the job in the queue
