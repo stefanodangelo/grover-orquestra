636f074750eebe9e41cf6a569aa3675641d0882f
stefanodangelo <61756862+stefanodangelo@users.noreply.github.com>
2021-06-29 15:22:37 +0200

    Update prova.py

--- steps/prova.py
+++ steps/prova.py
@@ -14,8 +14,8 @@ def run_and_measure(circuit = None, backend=None, save_path = 'measurements.json
     IBMQ.save_account("f1b53e81357761bafbbb2d7a71eaa26cfc0d9df4eeb53513096c5cee90ec01bcc8ba6fa547ea4988a1b9136f2abf8090a133546389581b3c51a1471dad8749e6")
     provider = IBMQ.load_account()
     backend = least_busy(provider.backends(filters=lambda x: x.configuration().n_qubits >= len(qc.qubits) and not x.configuration().simulator and x.status().operational == True))
-    """
-    # Run our circuit on the least busy backend. Monitor the execution of the job in the queue
+
+    # Run circuit on the least busy backend. Monitor the execution of the job in the queue
     transpiled_grover_circuit = transpile(qc, backend, optimization_level=3)
     qobj = assemble(transpiled_grover_circuit)
     job = backend.run(qobj)
@@ -23,8 +23,8 @@ def run_and_measure(circuit = None, backend=None, save_path = 'measurements.json
 
     # Get the results from the computation
     results = job.result()
-    answer = results.get_counts(qc)
-    """
+    answer['results'] = results.get_counts(qc)
+    
     with open(save_path, 'w') as f:
         json.dump(answer, f)
        
