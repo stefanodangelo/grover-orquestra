232ab639c01ec2e79695a74e7be611f19da6f282
stefanodangelo <61756862+stefanodangelo@users.noreply.github.com>
2021-06-29 19:22:23 +0200

    Update measure.py

--- steps/measure.py
+++ steps/measure.py
@@ -1,5 +1,5 @@
-from modules.utils import *
-from modules.functions import *
+from modules.utils import from_json
+from modules.functions import get_results_from_IBM, get_simulation_results
 import json
 
 
